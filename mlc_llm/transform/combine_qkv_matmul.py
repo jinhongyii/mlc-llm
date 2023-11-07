a10053aed61ccb58abf3a497cf687f9fb0720ec7
Hongyi Jin <jinhongyi02@gmail.com>
2023-11-06 19:24:02 -0500

    generalize combine qkv matmul

--- mlc_llm/transform/combine_qkv_matmul.py
+++ mlc_llm/transform/combine_qkv_matmul.py
@@ -39,7 +39,6 @@ def rewrite_tir(prim_func: tir.PrimFunc, param_rewrite_spec: ParamRewriteSpec) -
                         success = False
     
     post_order_visit(prim_func.body, fcollect_var)
-    print("loop var:", loop_var)
     if not success:
         return prim_func, False
     
@@ -97,119 +96,102 @@ def rewrite_tir(prim_func: tir.PrimFunc, param_rewrite_spec: ParamRewriteSpec) -
     )
     return new_func, True
 
+
+def combine_parallel_matmul(mod, func, num_parallel):
+    with PatternContext() as ctx:
+        pat_input = wildcard()
+        pat_weights = [wildcard() for _ in range(num_parallel)]
+        pat_matmul_gvar = GlobalVarPattern("(NT_)?matmul.*")
+        pat_matmul_outputs = []
+        for pat_w in pat_weights:
+            pat_o = is_op("relax.call_tir")(
+                pat_matmul_gvar,
+                TuplePattern([pat_input, pat_w]),
+                add_constraint=False
+            )
+            pat_matmul_outputs.append(pat_o)
+            pat_input.used_by(pat_o)
+            pat_w.used_by(pat_o)
+            
+    def rewriter(matchings, bindings):
+        # Extracting all the relax and TIR variables that we'll need
+
+        input = matchings[pat_input]
+        weights = [matchings[pat_w] for pat_w in pat_weights]
+        matmul_outputs = [matchings[pat_o] for pat_o in pat_matmul_outputs]
+        
+        matmul_gvar = bindings[matmul_outputs[0]].args[0]
+        assert matmul_gvar.name_hint.startswith("matmul") or matmul_gvar.name_hint.startswith("NT_matmul")
+        decode_gvar = bindings[weights[0]].args[0]
+        assert decode_gvar.name_hint.startswith("decode")
+        
+        weights_data_to_lift = []
+        weights_scale_to_lift = []
+        for w in weights:
+            w_data, w_scale = bindings[w].args[1]
+            w_data_to_lift, w_scale_to_lift = bindings[w_data].args[0], bindings[w_scale].args[0]
+            weights_data_to_lift.append(w_data_to_lift)
+            weights_scale_to_lift.append(w_scale_to_lift)
+        
+        concat_wqkv_data_can_lift = R.concat(weights_data_to_lift, axis=0)
+        concat_wqkv_scale_can_lift = R.concat(weights_scale_to_lift, axis=0)
+        concat_wqkv_data = R.stop_lift_params(concat_wqkv_data_can_lift)
+        concat_wqkv_scale = R.stop_lift_params(concat_wqkv_scale_can_lift)
+        
+        decode_combine_func_name = decode_gvar.name_hint + "_combine_matmul"
+        matmul_combine_func_name = matmul_gvar.name_hint + "_combine_matmul"
+        if not any(gvar.name_hint == decode_combine_func_name for gvar in mod.functions ):
+            decode_tir = mod[decode_gvar]
+            new_decode_tir, decode_rewrite_success = rewrite_tir(decode_tir, ParamRewriteSpec(param_index=0, dim=0, new_value=sum([w.struct_info.shape[0] for w in weights])))
+            assert decode_rewrite_success
+            mod[decode_combine_func_name] = new_decode_tir
+            new_decode_gvar = mod.get_global_var(decode_combine_func_name)
+            relax.expr._update_struct_info(new_decode_gvar, mod[decode_combine_func_name].struct_info)
+        else:
+            new_decode_gvar = mod.get_global_var(decode_combine_func_name)
+
+        if not any(gvar.name_hint == matmul_combine_func_name for gvar in mod.functions ):
+            matmul_tir = mod[matmul_gvar]
+            new_matmul_tir, matmul_rewrite_success = rewrite_tir(matmul_tir, ParamRewriteSpec(param_index=1, dim=0, new_value=sum([w.struct_info.shape[0] for w in weights])))
+            assert matmul_rewrite_success
+            mod[matmul_combine_func_name] = new_matmul_tir
+            new_matmul_gvar = mod.get_global_var(matmul_combine_func_name)
+            relax.expr._update_struct_info(new_matmul_gvar, mod[matmul_combine_func_name].struct_info)
+        else:
+            new_matmul_gvar = mod.get_global_var(matmul_combine_func_name)
+            
+        concat_wqkv_shape = list(weights[0].struct_info.shape)
+        concat_wqkv_shape[0] = sum([w.struct_info.shape[0] for w in weights])
+        concat_wqkv_sinfo = R.Tensor(concat_wqkv_shape, dtype="float16")
+        concat_wqkv = R.call_tir(
+            new_decode_gvar,
+            (concat_wqkv_data, concat_wqkv_scale),
+            out_sinfo=concat_wqkv_sinfo
+        )
+        concat_qkv_shape = list(matmul_outputs[0].struct_info.shape)
+        concat_qkv_shape[-1] = sum([o.struct_info.shape[-1] for o in matmul_outputs])
+        concat_qkv_sinfo = R.Tensor(concat_qkv_shape, dtype="float16")
+        concat_qkv = R.call_tir(
+            new_matmul_gvar,
+            (input, concat_wqkv),
+            out_sinfo=concat_qkv_sinfo
+        )
+        split_idx = [sum([o.struct_info.shape[-1] for o in matmul_outputs[:i]]) for i in range(1, len(matmul_outputs))]
+        qkv_tuple = R.split(concat_qkv, split_idx, axis=-1)
+        out_dict = {matmul_outputs[i]: R.TupleGetItem(qkv_tuple, i) for i in range(len(matmul_outputs))}
+        
+        return out_dict
+    func = rewrite_bindings(ctx, rewriter, func)
+    return func
+
 def combine_qkv():
     @tvm.ir.transform.module_pass(opt_level=0, name="fuse_split_rotary_embedding")
     def ir_module_pass(mod: tvm.IRModule, _pass_context) -> tvm.IRModule:
-        with PatternContext() as ctx:
-            pat_input = wildcard()
-            pat_wq = wildcard()
-            pat_wk = wildcard()
-            pat_wv = wildcard()
-            pat_matmul_gvar = GlobalVarPattern("(NT_)?matmul.*")
-            pat_q = is_op("relax.call_tir")(
-                pat_matmul_gvar,
-                TuplePattern([pat_input, pat_wq]),
-                add_constraint=False
-            )
-            pat_input.used_by(pat_q)
-            pat_wq.used_by(pat_q)
-            pat_k = is_op("relax.call_tir")(
-                pat_matmul_gvar,
-                TuplePattern([pat_input, pat_wk]),
-                add_constraint=False
-            )
-            pat_input.used_by(pat_k)
-            pat_wk.used_by(pat_k)
-            pat_v = is_op("relax.call_tir")(
-                pat_matmul_gvar,
-                TuplePattern([pat_input, pat_wv]),
-                add_constraint=False
-            )
-            pat_wv.used_by(pat_v)
-            pat_input.used_by(pat_v)
-
-        def rewriter(matchings, bindings):
-            # Extracting all the relax and TIR variables that we'll need
-
-            input = matchings[pat_input]
-
-            wq = matchings[pat_wq]
-            wk = matchings[pat_wk]
-            wv = matchings[pat_wv]
-            q = matchings[pat_q]
-            k = matchings[pat_k]
-            v = matchings[pat_v]
-            
-            matmul_gvar = bindings[q].args[0]
-            assert matmul_gvar.name_hint.startswith("matmul") or matmul_gvar.name_hint.startswith("NT_matmul")
-            decode_gvar = bindings[wq].args[0]
-            assert decode_gvar.name_hint.startswith("decode")
-            
-            wq_data, wq_scale = bindings[wq].args[1]
-            wq_data_can_lift, wq_scale_can_lift = bindings[wq_data].args[0], bindings[wq_scale].args[0]
-            wk_data, wk_scale = bindings[wk].args[1]
-            wk_data_can_lift, wk_scale_can_lift = bindings[wk_data].args[0], bindings[wk_scale].args[0]
-            wv_data, wv_scale = bindings[wv].args[1]
-            wv_data_can_lift, wv_scale_can_lift = bindings[wv_data].args[0], bindings[wv_scale].args[0]
-            
-            concat_wqkv_data_can_lift = R.concat([wq_data_can_lift, wk_data_can_lift, wv_data_can_lift], axis=0)
-            concat_wqkv_scale_can_lift = R.concat([wq_scale_can_lift, wk_scale_can_lift, wv_scale_can_lift], axis=0)
-            concat_wqkv_data = R.stop_lift_params(concat_wqkv_data_can_lift)
-            concat_wqkv_scale = R.stop_lift_params(concat_wqkv_scale_can_lift)
-            
-            decode_combine_func_name = decode_gvar.name_hint + "_combine_qkv"
-            matmul_combine_func_name = matmul_gvar.name_hint + "_combine_qkv"
-            if not any(gvar.name_hint == decode_combine_func_name for gvar in mod.functions ):
-                decode_tir = mod[decode_gvar]
-                new_decode_tir, decode_rewrite_success = rewrite_tir(decode_tir, ParamRewriteSpec(param_index=0, dim=0, new_value=wq.struct_info.shape[0] + wk.struct_info.shape[0] + wv.struct_info.shape[0]))
-                assert decode_rewrite_success
-                mod[decode_combine_func_name] = new_decode_tir
-                new_decode_gvar = mod.get_global_var(decode_combine_func_name)
-                relax.expr._update_struct_info(new_decode_gvar, mod[decode_combine_func_name].struct_info)
-            else:
-                new_decode_gvar = mod.get_global_var(decode_combine_func_name)
-
-            if not any(gvar.name_hint == matmul_combine_func_name for gvar in mod.functions ):
-                matmul_tir = mod[matmul_gvar]
-                new_matmul_tir, matmul_rewrite_success = rewrite_tir(matmul_tir, ParamRewriteSpec(param_index=1, dim=0, new_value=wq.struct_info.shape[0] + wk.struct_info.shape[0] + wv.struct_info.shape[0]))
-                assert matmul_rewrite_success
-                mod[matmul_combine_func_name] = new_matmul_tir
-                new_matmul_gvar = mod.get_global_var(matmul_combine_func_name)
-                relax.expr._update_struct_info(new_matmul_gvar, mod[matmul_combine_func_name].struct_info)
-            else:
-                new_matmul_gvar = mod.get_global_var(matmul_combine_func_name)
-                
-            concat_wqkv_shape = list(wq.struct_info.shape)
-            concat_wqkv_shape[0] = wq.struct_info.shape[0] + wk.struct_info.shape[0] + wv.struct_info.shape[0]
-            concat_wqkv_sinfo = R.Tensor(concat_wqkv_shape, dtype="float16")
-            concat_wqkv = R.call_tir(
-                new_decode_gvar,
-                (concat_wqkv_data, concat_wqkv_scale),
-                out_sinfo=concat_wqkv_sinfo
-            )
-            concat_qkv_shape = list(q.struct_info.shape)
-            concat_qkv_shape[-1] = q.struct_info.shape[-1] + k.struct_info.shape[-1] + v.struct_info.shape[-1]
-            concat_qkv_sinfo = R.Tensor(concat_qkv_shape, dtype="float16")
-            concat_qkv = R.call_tir(
-                new_matmul_gvar,
-                (input, concat_wqkv),
-                out_sinfo=concat_qkv_sinfo
-            )
-            qkv_tuple = R.split(concat_qkv, [q.struct_info.shape[-1], q.struct_info.shape[-1] + k.struct_info.shape[-1]], axis=-1)
-            new_q = R.TupleGetItem(qkv_tuple, 0)
-            new_k = R.TupleGetItem(qkv_tuple, 1)
-            new_v = R.TupleGetItem(qkv_tuple, 2)
-            
-            return {
-                q: new_q,
-                k: new_k,
-                v: new_v,
-            }
-
         new_mod = {}
         for gvar, func in mod.functions.items():
             if isinstance(func, relax.Function):
-                func = rewrite_bindings(ctx, rewriter, func)
+                func = combine_parallel_matmul(mod, func, num_parallel=3)
+                func = combine_parallel_matmul(mod, func, num_parallel=2)
             new_mod[gvar] = func
             
         for gvar, func in mod.functions.items():
